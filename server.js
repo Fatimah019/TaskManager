6fc2aad34bf7f92c700d7abd2a9dd27a1ac168bf
Fatimah <daviesfatimah97@gmail.com>
2021-04-13 04:41:52 -0700

    .

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ mongoose
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static("build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
